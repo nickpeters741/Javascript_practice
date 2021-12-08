a84ccefa5202662b069f9f9a7ccebe5cbd4dc6bc
nickpeters741 <npeters741@gmail.com>
2021-12-08 01:16:18 -0800

    linking js file to html

---
+++ Manipulate-the-DOM/home.js
@@ -0,0 +1,14 @@
+console.log('hello');
+alert('stop!');
+//this is how we comment
+ 
+//variables
+var b = 'nick';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var Name = prompt('what is your Name?');
+document.getElementById('someText').innerHTML = 'hello world';
+document.getElementById('someText').innerHTML = Name;
